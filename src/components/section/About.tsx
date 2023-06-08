15173bded01af92440ae839f8219ac02c391f152
Edward Opoku-Agyemang <oaedward83@gmail.com>
2023-06-08 18:05:34 +0000

    v1 about page completed

--- src/components/section/About.tsx
+++ src/components/section/About.tsx
@@ -1,10 +1,36 @@
+import Image from "next/image";
+
 export default function About() {
   return (
     <section
       id="about"
-      className="w-full pt-20 pb-24 p-4 flex flex-col gap-10 xl:gap-0 lg:flex-row align-middle items-center border-b-[1px] border-b-black"
+      className="w-full xl:pt-20 xl:pb-24 pb-0 p-4 flex flex-col-reverse gap-10 xl:gap-0 xl:flex-row align-middle items-center border-b-[1px] border-b-black"
     >
-      <h1>happy banner</h1>
+      <div className=" w-full lg:w-1/2 flex justify-center items-center relative dark:hidden">
+        <Image
+          src="/assets/3d_character/black_hoodie_3.png"
+          width={560}
+          height={300}
+          alt="hero"
+        />
+      </div>
+      <div className="w-full md:w-3/4 lg:w-1/2 flex flex-col gap-10 ">
+        <div className="flex flex-col gap-5 pt-24 md:pt-24 xl:pt-0">
+          <h4 className="block text-md font-bold text-gray-500 dark:text-gray-50 md:text-center text-center xl:text-left">
+            All there is to know <span className="text-red-600">about us</span>{" "}
+            is that
+          </h4>
+          <h1 className="text-4xl md:text-6xl xl:text-6xl font-bold text-gray-700 dark:text-white md:text-center text-center xl:text-left">
+            We build lasting and successful projects
+          </h1>
+          <p className="container-xl text-sm font-normal text-gray-400 text-center md:text-center xl:text-left">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
+            laboriosam, incidunt cumque nam fugiat optio vero ad repellendus
+            autem possimus modi quisquam pariatur vel magnam illum impedit
+            similique! Quaerat, molestiae!
+          </p>
+        </div>
+      </div>
     </section>
   );
 }
